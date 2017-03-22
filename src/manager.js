4f67dee2d508db6076edc7a00ce3153e6d04164c
Joon Park <parkjoon@umich.edu>
2017-03-22 01:46:27 -0400

    Added port customization.

--- src/manager.js
+++ src/manager.js
@@ -4,6 +4,8 @@ import { getRelevantRoomNames } from './utilities/manager';
 export default function initializeLogManager(io) {
 	console.log('Initializing log manager listeners...');
 	io.on(EVENTS.CONNECTION, socket => {
+		console.log(`Received event: connection from socket id: ${socket.id}`);
+
 		socket.on(EVENTS.GET_APPLICATIONS, () => {
 			console.log(`Received event: GET_APPLICATIONS from socket id: ${socket.id}`);
 			socket.emit(EVENTS.SET_APPLICATIONS, APPLICATIONS);
@@ -15,7 +17,7 @@ export default function initializeLogManager(io) {
 		});
 
 		socket.on(EVENTS.LOG, log => {
-			console.log(`Received event: LOG from socket id: ${socket.id}`);
+			console.log(`Received event: LOG from socket id: ${socket.id} with log time: ${log.time}`);
 			const relevantRoomNames = getRelevantRoomNames(log.application, log.type);
 			relevantRoomNames.forEach(relevantRoomName => {
 				io.to(relevantRoomName).emit(EVENTS.LOG, log);
@@ -23,12 +25,12 @@ export default function initializeLogManager(io) {
 		});
 
 		socket.on(EVENTS.JOIN_ROOM, roomName => {
-			console.log(`Received event: JOIN_ROOM from socket id: ${socket.id}`);
+			console.log(`Received event: JOIN_ROOM from socket id: ${socket.id} for room: ${roomName}`);
 			socket.join(roomName);
 		});
 
 		socket.on(EVENTS.LEAVE_ROOM, roomName => {
-			console.log(`Received event: LEAVE_ROOM from socket id: ${socket.id}`);
+			console.log(`Received event: LEAVE_ROOM from socket id: ${socket.id} for room: ${roomName}`);
 			socket.leave(roomName);
 		});
 	});
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import initializeLogManager from './manager';
 
+const config = require('./config.json');
 const app = require('http').createServer();
 const io = require('socket.io')(app);
 
-app.listen(8080, () => {
-	console.log('SocketIO log manager is now running on port 8080.');
+app.listen(config.port || 8080, () => {
+	console.log(`SocketIO log manager is now running on port ${config.port || 8080}.`);
 	initializeLogManager(io);
 });
