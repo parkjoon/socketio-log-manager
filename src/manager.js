6128f3bb199c018d0e6ae9a5a77b95f594794fe7
Joon Park <parkjoon@umich.edu>
2017-03-21 20:40:38 -0400

    Created events for applications data.

--- src/manager.js
+++ src/manager.js
@@ -1,18 +1,26 @@
-import { EVENTS, ROOMTYPES, TYPES } from './utilities/constants';
+import { APPLICATIONS, EVENTS, ROOMTYPES, TYPES } from './utilities/constants';
 import { getRelevantRoomNames } from './utilities/manager';
 
 export default function initializeLogManager(io) {
 	console.log('Initializing log manager listeners...');
 	io.on(EVENTS.CONNECTION, socket => {
+		socket.on(EVENTS.GET_APPLICATIONS, () => {
+			console.log(`Received event: GET_APPLICATIONS from socket id: ${socket.id}`);
+			socket.emit(EVENTS.SET_APPLICATIONS, APPLICATIONS);
+		});
+
 		socket.on(EVENTS.GET_ROOMTYPES, () => {
+			console.log(`Received event: GET_ROOMTYPES from socket id: ${socket.id}`);
 			socket.emit(EVENTS.SET_ROOMTYPES, ROOMTYPES);
 		});
 
 		socket.on(EVENTS.GET_TYPES, () => {
+			console.log(`Received event: GET_TYPES from socket id: ${socket.id}`);
 			socket.emit(EVENTS.SET_TYPES, TYPES);
 		});
 
 		socket.on(EVENTS.LOG, log => {
+			console.log(`Received event: LOG from socket id: ${socket.id}`);
 			const relevantRoomNames = getRelevantRoomNames(log.application, log.type);
 			relevantRoomNames.forEach(relevantRoomName => {
 				io.to(relevantRoomName).emit(log);
@@ -20,10 +28,12 @@ export default function initializeLogManager(io) {
 		});
 
 		socket.on(EVENTS.JOIN_ROOM, roomName => {
+			console.log(`Received event: JOIN_ROOM from socket id: ${socket.id}`);
 			socket.join(roomName);
 		});
 
 		socket.on(EVENTS.LEAVE_ROOM, roomName => {
+			console.log(`Received event: LEAVE_ROOM from socket id: ${socket.id}`);
 			socket.leave(roomName);
 		});
 	});
--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,11 +1,24 @@
+export const APPLICATIONS = [
+	{
+		id: 'FORM_BUILDER',
+		label: 'Form Builder'
+	},
+	{
+		id: 'REPORT_FORM_BUILDER',
+		label: 'Report Form Builder'
+	}
+];
+
 // SocketIO events.
 export const EVENTS = {
 	CONNECTION: 'connection',
+	GET_APPLICATIONS: 'GET_APPLICATIONS',
 	GET_ROOMTYPES: 'GET_ROOMTYPES',
 	GET_TYPES: 'GET_TYPES',
 	LEAVE_ROOM: 'LEAVE_ROOM',
 	LOG: 'LOG',
 	JOIN_ROOM: 'JOIN_ROOM',
+	SET_APPLICATIONS: 'SET_APPLICATIONS',
 	SET_ROOMTYPES: 'SET_ROOMTYPES',
 	SET_TYPES: 'SET_TYPES'
 };
